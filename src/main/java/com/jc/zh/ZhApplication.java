4a617e7b04d0dca10c2f14fb6855698d4c82d43b
Your Name <389453775@qq.com>
2022-05-04 22:03:00 +0800

    pastJava

---
+++ src/main/java/com/jc/zh/ZhApplication.java
@@ -0,0 +1,13 @@
+package com.jc.zh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZhApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZhApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jc/zh/ZhApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jc.zh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZhApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
